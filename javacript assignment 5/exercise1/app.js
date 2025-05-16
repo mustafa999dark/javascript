fdc7be3c1cb51154db95c8f43f22ce283f60cc7b
Mustafa Ali <mustafaali467373@gmail.com>
2025-05-16 19:47:14 +0500

    new

---
+++ javacript assignment 5/exercise1/app.js
@@ -0,0 +1,110 @@
+var products = [
+  {
+    id: 101,
+    title: "Sony LED 40 inch",
+    variations: [
+      { id: 1, color: "black", price: 50000, quantity: 5 },
+      { id: 2, color: "red", price: 50000, quantity: 1 },
+      { id: 3, color: "silver", price: 55000, quantity: 8 },
+    ],
+    reviews: [
+      {
+        id: 1,
+        user: "Ahmad",
+        rating: 4.0,
+        title: "Good Product",
+        comments: "It is a very good product ....",
+        date: "06-02-2021",
+        status: true,
+      },
+      {
+        id: 2,
+        user: "Zubair",
+        rating: 4.5,
+        title: "Very Good Product",
+        comments: "zubair It is a very good product ....",
+        date: "05-02-2021",
+        status: false,
+      },
+      {
+        id: 3,
+        user: "Ali",
+        rating: 5.0,
+        title: "bad Product",
+        comments: "ali It is a very good product ....",
+        date: "04-02-2021",
+        status: true,
+      },
+    ],
+  },
+  {
+    id: 102,
+    title: "Mobile",
+    variations: [
+      { id: 1, color: "black", price: 50000, quantity: 5 },
+      { id: 2, color: "red", price: 50000, quantity: 1 },
+      { id: 3, color: "silver", price: 55000, quantity: 8 },
+    ],
+    reviews: [
+      {
+        id: 1,
+        user: "Ahmad",
+        rating: 4.0,
+        title: "Good Product",
+        comments: "It is a very good product ....",
+        date: "06-02-2021",
+        status: true,
+      },
+      {
+        id: 2,
+        user: "Zubair",
+        rating: 4.5,
+        title: "Very Good Product",
+        comments: "zubair It is a very good product ....",
+        date: "05-02-2021",
+        status: false,
+      },
+      {
+        id: 3,
+        user: "Ali",
+        rating: 5.0,
+        title: "bad Product",
+        comments: "ali It is a very good product ....",
+        date: "04-02-2021",
+        status: true,
+      },
+    ],
+  },
+  {
+    id: 103,
+    title: "Bike",
+    variations: [
+      { id: 1, color: "black", price: 55000, quantity: 5 },
+      { id: 2, color: "red", price: 50000, quantity: 1 },
+    ],
+    reviews: [
+      {
+        id: 1,
+        user: "Ahmad",
+        rating: 4.0,
+        title: "Good Product",
+        comments: "It is a very good product ....",
+        date: "06-02-2021",
+        status: true,
+      },
+      {
+        id: 2,
+        user: "Zubair",
+        rating: 3.0,
+        title: "Very Good Product",
+        comments: "zubair It is a very good product ....",
+        date: "05-02-2021",
+        status: false,
+      },
+    ],
+  },
+];
+
+
+let productTitle = products.map(products =>products.title);
+console.log(productTitle);
